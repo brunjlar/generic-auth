d2e6fa6624a16ce7fa6c2f9256874d1ae61a278e
Lars Bruenjes <brunjlar@gmail.com>
2017-08-26 11:08:55 +0200

    first basic definitions

--- src/Data/Auth.hs
+++ src/Data/Auth.hs
@@ -1,3 +1,63 @@
 module Data.Auth
-    (
+    ( Auth
+    , Authenticatable (..)
+    , authP
+    , authV
+    , unauthP
+    , unauthV
     ) where
+
+import Data.Auth.Hash
+import Data.Binary    (Binary)
+import GHC.Generics   (Generic)
+
+data Auth a =
+      Prover Hash a
+    | Verifier Hash
+    deriving (Generic, Show)
+
+instance Binary a => Binary (Auth a) where
+
+class Binary a => Authenticatable a where
+
+    shallowCopy :: a -> a
+    shallowCopy = id
+
+instance Authenticatable Char where
+
+instance Authenticatable Hash where
+
+instance Authenticatable a => Authenticatable [a] where
+
+    shallowCopy = map shallowCopy
+
+instance (Authenticatable a, Authenticatable b) => Authenticatable (a, b) where
+
+    shallowCopy (a, b) = (shallowCopy a, shallowCopy b)
+
+instance (Authenticatable a, Authenticatable b) => Authenticatable (Either a b) where
+
+    shallowCopy = either (Left . shallowCopy) (Right . shallowCopy)
+
+instance Binary a => Authenticatable (Auth a) where
+
+    shallowCopy (Prover h _)   = Verifier h
+    shallowCopy v@(Verifier _) = v
+
+hashAuth :: Authenticatable a => a -> Hash
+hashAuth = hash . shallowCopy
+
+authP :: Authenticatable a => a -> Auth a
+authP a = Prover (hashAuth a) a
+
+authV ::  Authenticatable a => a -> Auth a
+authV = Verifier . hashAuth
+
+unauthP :: Authenticatable a => Auth a -> a
+unauthP (Prover _ a) = a
+unauthP (Verifier _) = error "expected Prover"
+
+unauthV :: Authenticatable a => Auth a -> a -> a
+unauthV (Prover _ _) _ = error "expected Verifier"
+unauthV (Verifier h) a = if h == hash a then a
+                                        else error "verification error"
---
+++ src/Data/Auth/Hash.hs
@@ -0,0 +1,31 @@
+module Data.Auth.Hash
+    ( Hash
+    , hash
+    ) where
+
+import qualified Crypto.Hash          as C
+import           Data.Binary          (Binary (..), encode)
+import qualified Data.ByteArray       as A
+import           Data.ByteString.Lazy (toStrict)
+import qualified Data.ByteString      as B
+
+newtype Hash = Hash {getHash :: C.Digest C.SHA256}
+    deriving Eq
+
+instance Show Hash where
+
+    show = show . getHash
+
+instance Binary Hash where
+
+    get = do
+        md <- (C.digestFromByteString . B.pack) <$> get
+        case md of
+            Nothing -> fail "expected hash"
+            Just d  -> return $ Hash d
+
+    put = put . A.unpack . getHash
+
+
+hash :: Binary a => a -> Hash
+hash = Hash . C.hash . toStrict . encode
